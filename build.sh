156fd3053c4f837a890ccbea1704c6bb6ab7b979
in2ja <in2jaram@gmail.com>
2026-07-24 15:53:27 +0530

    Use Django createsuperuser

--- build.sh
+++ build.sh
@@ -7,4 +7,8 @@ python manage.py migrate
 
 python manage.py collectstatic --noinput
 
-python manage.py createsuperuser --noinput || true
+echo "Creating superuser..."
+
+python manage.py createsuperuser --noinput
+
+echo "Finished creating superuser."
